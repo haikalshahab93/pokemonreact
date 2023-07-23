80d270c58eea0ec565f3d4e58dbac93bf52e4d3e
haikalshahab93 <haikal.pusri@gmail.com>
2023-07-23 17:43:44 +0700

    layout navbar wrapper footer

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import pokeballicon from "../assets/pokeball-icon.png"
+import {} from 'react-icons/gi'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className="block">
+      <img src={pokeballicon} alt="pokeballicon" />
+      </div>
+      <div className="data">
+
+      </div>
+      <div className="block">
+
+      </div>
+    </nav>
   )
 }
 
