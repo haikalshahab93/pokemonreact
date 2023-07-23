ddad5b7ce6ae27875ed3741d983b5030d81aacd6
haikalshahab93 <haikal.pusri@gmail.com>
2023-07-23 16:11:23 +0700

    update backgorund

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Wrapper from "./sections/Wrapper";
+import Footer from "./sections/Footer";
+import Background from "./components/Background";
+import "./scss/index.scss";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="main-container">
+      <Background/>
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/sections/Wrapper.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Wrapper() {
+  return (
+    <div className='content'>Wrapper</div>
+  )
+}
+
+export default Wrapper
